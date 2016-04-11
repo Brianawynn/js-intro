04a1453fb214cae7f6f9029f27d204a0fe4e55f9
Brianawynn <wynn.briana@wustl.edu>
2016-04-11 18:50:15 -0500

    Update scripts.js

--- 04_vars/scripts.js
+++ 04_vars/scripts.js
@@ -1,6 +1,8 @@
 var content = "";
+var heading = "";
+heading+= "This heading";
 
-content += "<h1> this heading</h1>";
+content += "<h1>" + heading + "</h1>";
 
 content += "<p> this is a paragraph with a few words that repeat a few times</p>"
 
