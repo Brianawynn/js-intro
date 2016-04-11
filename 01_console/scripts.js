f7026a4339e47575061c842ae665b1a59018fdf1
Brianawynn <wynn.briana@wustl.edu>
2016-04-11 18:28:16 -0500

    Update scripts.js

--- 01_console/scripts.js
+++ 01_console/scripts.js
@@ -1,7 +1,7 @@
 alert("Help me fix this program!");
 alert("Can you get this message to appear?");
 document.write("<h2>My first JavaScript program</h2>");
-document.wrong("I'm practicing 'debugging'.");
+document.write("I'm practicing 'debugging'.");
  
  
  
