7465e9383566a7825e2cb533e1aef386fd617974
Brianawynn <wynn.briana@wustl.edu>
2016-04-11 18:42:23 -0500

    Update scripts.js

--- 03_paragraph/scripts.js
+++ 03_paragraph/scripts.js
@@ -1,1 +1,4 @@
-document.write("Paragraph");
+document.write("<h1> Heading </h1>");
+document.write("<p> paragraph with a few words repeated a few times</p>");
+document.write("<p> paragraph with a few words repeated a few times</p>");
+document.write("<p> paragraph with a few words repeated a few times</p>");
